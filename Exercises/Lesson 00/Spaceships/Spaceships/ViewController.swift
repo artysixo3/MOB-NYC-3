bd7d3e7a492c191f96f6b11c0bf456fc7cbcd664
artysixo3 <art@Arts-MacBook-Pro.local>
2015-04-21 21:32:08 -0400

    Added my exercise to lesson 0

---
+++ Exercises/Lesson 00/Spaceships/Spaceships/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spaceships
+//
+//  Created by Art Rosario on 4/19/15.
+//  Copyright (c) 2015 Art Rosario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
